71dfbdf1fafcc1a01fa555679fce75d10d65aa95
jan <jan@jans-Virtual-Machine.local>
2023-07-19 14:55:27 +0200

    init

---
+++ modules/darwin/default.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: {
+  # here go the darwin preferences and config items
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+  environment = {
+    shells = with pkgs; [ bash zsh fish ];
+    loginShell = pkgs.fish;
+    systemPackages = [ pkgs.coreutils ];
+    systemPath = [ "/opt/homebrew/bin" ];
+    pathsToLink = [ "/Applications" ];
+  };
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+  fonts.fontDir.enable = false; #true; # DANGER
+  fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+  services.nix-daemon.enable = true;
+  system.defaults = {
+    finder.AppleShowAllExtensions = true;
+    finder._FXShowPosixPathInTitle = true;
+    dock.autohide = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.InitialKeyRepeat = 14;
+    NSGlobalDomain.KeyRepeat = 1;
+  };
+  # backwards compat; don't change
+  system.stateVersion = 4;
+  homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    masApps = { };
+    casks = [ "raycast" "amethyst" ];
+    # taps = [ "fujiapple852/trippy" ];
+    # brews = [ "trippy" ];
+  };
+}
