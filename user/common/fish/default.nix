42a99fc4cbdf96c44eae983fd013d919f5e3880d
Jan Laemrann <laermannjan@gmail.com>
2024-04-08 07:07:31 +0200

    get rid of fzf color duplicate

--- user/common/fish/default.nix
+++ user/common/fish/default.nix
@@ -39,11 +39,6 @@ in
       # `ssh-add -l` will now show all available ssh-keys from 1password
       set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
-      # catppuccin fzf
-      set -Ux FZF_DEFAULT_OPTS "\
-      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-      --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
       # name: 'Catppuccin mocha'
       # url: 'https://github.com/catppuccin/fish'
