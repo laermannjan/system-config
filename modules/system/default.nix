39f6ef805192d2126fe76544972cea5ac9bd5844
Jan Laemrann <laermannjan@gmail.com>
2024-04-24 01:22:59 +0200

    update

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -4,13 +4,29 @@
   lib,
   inputs,
   outputs,
-  myLib,
+  utils,
   ...
-}: {
-  imports = [
-    inputs.home-manager.darwinModules.home-manager
-    # inputs.home-manager.nixosModules.home-manager
-  ];
+}: let
+  cfg = config.myNixOS;
+
+  # Taking all modules in ./features and adding enables to them
+  features =
+    utils.extendModules
+    (name: {
+      extraOptions = {
+        myNixOS.${name}.enable = lib.mkEnableOption "enable my ${name} configuration";
+      };
+
+      configExtension = config: (lib.mkIf cfg.${name}.enable config);
+    })
+    (utils.filesIn ./features);
+in {
+  imports =
+    [
+      inputs.home-manager.darwinModules.home-manager
+      # inputs.home-manager.nixosModules.home-manager
+    ]
+    ++ features;
 
   config = {
     nix.settings.experimental-features = ["nix-command" "flakes"];
